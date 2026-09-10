ff676eb34ef75b3bb18bfdc594648c0a0a2a632d
M1chael33 <walshmike2007@gmail.com>
2026-09-09 20:03:22 -0400

    added check of task func

--- main.go
+++ main.go
@@ -4,14 +4,67 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type data struct {
-	task string
+	task      string
+	isChecked bool
 }
 
 var tasks []data
+
+func addTask(parts []string) {
+	if len(parts) < 2 {
+		fmt.Println("Usage: add <task>")
+		return
+	}
+
+	task := strings.Join(parts[1:], " ")
+	tasks = append(tasks, data{task: task, isChecked: false})
+	fmt.Println("Added:", task)
+}
+
+func listTasks() {
+	maxLen := 0
+	for _, t := range tasks {
+		if len(t.task) > maxLen {
+			maxLen = len(t.task)
+		}
+	}
+
+	for i, t := range tasks {
+		checkbox := "[ ]"
+		if t.isChecked {
+			checkbox = "[x]"
+		}
+		fmt.Printf("%d. %-*s %s\n", i+1, maxLen+2, t.task, checkbox)
+	}
+}
+
+func checkTask(parts []string) {
+	if len(parts) < 2 {
+		fmt.Println("Usage: check <task number>")
+		return
+	}
+
+	num, err := strconv.Atoi(parts[1])
+	if err != nil {
+		fmt.Println("Error: Please provide a valid number")
+		return
+	}
+
+	index := num - 1
+
+	if index < 0 || index >= len(tasks) {
+		fmt.Println("Error: Task number does not exist")
+		return
+	}
+
+	tasks[index].isChecked = true
+	fmt.Println("Marked done:", tasks[index].task)
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -21,7 +74,7 @@ func main() {
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
 
-		parts := strings.Fields(input) // splits on whitespace
+		parts := strings.Fields(input)
 		if len(parts) == 0 {
 			continue
 		}
@@ -30,23 +83,14 @@ func main() {
 
 		switch command {
 		case "add":
-			if len(parts) < 2 {
-				fmt.Println("Usage: add <task>")
-				continue
-			}
-			task := strings.Join(parts[1:], " ")
-			tasks = append(tasks, data{task: task})
-			fmt.Println("Added:", task)
-
+			addTask(parts)
 		case "list":
-			for i, t := range tasks {
-				fmt.Printf("%d: %s\n", i, t.task)
-			}
-
+			listTasks()
+		case "check":
+			checkTask(parts)
 		case "exit", "quit":
 			fmt.Println("Bye!")
-			return // exits main, ending the program
-
+			return
 		default:
 			fmt.Println("Unknown command:", command)
 		}
